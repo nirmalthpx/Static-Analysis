2360583ce890be83f2c0b5d8e86ae8666ccb546c
nirmalthpx <nirmal.thpx@gmail.com>
2025-01-28 08:06:31 +0545

    Update and rename  solution 5 (Using Sootup) to Analyse.java

---
+++ solution 5 (Using Sootup) /Analyse.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import static org.example.FunctionGraph.generateGraph;
+
+public class Analyse {
+
+    public static void main(String[] args) {
+
+        generateGraph();
+
+    }
+
+}
